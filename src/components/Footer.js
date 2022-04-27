88d56be68c189143d0daea96a1a0527abc69a55d
Magdalena Perry <magperry16@gmail.com>
2022-04-27 18:40:19 -0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,43 @@
 import React from 'react';
-import { FaFileExport, FaMapMarkerAlt } from "react-icons/fa";
+import { FaMapMarkerAlt } from "react-icons/fa";
 import { FaPhone } from "react-icons/fa";
 import { FaEnvelope } from "react-icons/fa";
+import { FaGithub } from 'react-icons/fa';
 
 const styles = {
   footer: {
-    display: "flex",
-    position: "absolute",
-    backgroundColor: "red"
+    position: "fixed",
+    bottom: 0,
+    backgroundColor: "rgba(211, 211, 211, 0.105)"
   }
 }
 
 export default function Footer() {
   return (
-  <>
+    <>
       <footer className="text-center col-12" style={styles.footer}>
-        <div className="col-12 text-center">
-            < FaMapMarkerAlt />
-            <p>Charlotte, NC</p>
+        <div className="row text-center text-secondary py-4">
+          <div className='text-center'>
+            <span className="mx-4">
+              <a className="col-12 text-decoration-none text-secondary"> < FaMapMarkerAlt /> Charlotte, NC</a>
+            </span>
+            <span className="mx-4">
+              <a className="col-12 text-decoration-none text-secondary" href="tel:1234567890"><FaPhone /> (704)609-7506</a>
+            </span>
+            <span className="me-4">
+              <a className="col-12 text-decoration-none text-secondary" href="mailto:mageltron@gmail.com"><FaEnvelope />mageltron@gmail.com</a>
+            </span>
+            <span className="me-4">
+              <a className="col-12 text-decoration-none text-secondary" href="https://github.com/magdalenaperry"><FaGithub />magdalenaperry</a>
+            </span>
+          </div>
 
-              <FaPhone />
-              <a className="text-decoration-none text-secondary" href="tel:1234567890">(123) 456-7890 </a>
-
-              <FaEnvelope />
-              <a className="text-decoration-none text-secondary" href="mailto:mageltron@gmail.com">mageltron@gmail.com</a>
         </div>
-   
-    </footer>
+      </footer>
 
-  
-  
-  </>
+
+
+    </>
   );
 
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ResumePDF from '../../images/magdalena-perry-resume.pdf'
 
 export default function Resume() {
   return (
     <>
     
-
+      <a href={ResumePDF} download><span>resume</span></a>
 
 
 
