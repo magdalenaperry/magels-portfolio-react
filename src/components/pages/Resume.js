d783b302ca1c5e7647d13c7ceb910f201a959410
Magdalena Perry <magperry16@gmail.com>
2022-04-27 20:50:34 -0400

    update padding in resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,23 +2,32 @@ import React from 'react';
 import ResumePDF from '../../images/magdalena-perry-resume.pdf'
 import { FaDownload } from 'react-icons/fa';
 
+const styles = {
+  padding: {
+    paddingBottom: "100px"
+  }
+}
+
 export default function Resume() {
   return (
     <>
-      <ul className='list-group container'>
-        <h2 class="titles mt-5 text-uppercase text-center"> Proficiencies:</h2>
-        <li className='list-group-item'>HTML</li>
-        <li className='list-group-item'>CSS</li>
-        <li className='list-group-item'>JavaScript</li>
-        <li className='list-group-item'>MVC</li>
-        <li className='list-group-item'>Restful API</li>
-        <li className='list-group-item'>MYSQL</li>
-        <li className='list-group-item'>Express</li>
-        <li className='list-group-item'>React</li>
-        <li className='list-group-item'>MongoDB</li>
-        <li className='list-group-item'><a href={ResumePDF} download><span> <FaDownload/> Resume </span></a></li>
+    <div style={styles.padding}>
+        <ul className='list-group container pb-5' >
+          <h2 class="titles mt-5 text-uppercase text-center"> Proficiencies:</h2>
+          <li className='list-group-item'>HTML</li>
+          <li className='list-group-item'>CSS</li>
+          <li className='list-group-item'>JavaScript</li>
+          <li className='list-group-item'>MVC</li>
+          <li className='list-group-item'>Restful API</li>
+          <li className='list-group-item'>MYSQL</li>
+          <li className='list-group-item'>Express</li>
+          <li className='list-group-item'>React</li>
+          <li className='list-group-item'>MongoDB</li>
+          <li className='list-group-item'><a href={ResumePDF} download><span> <FaDownload /> Resume </span></a></li>
 
-      </ul>
+        </ul>
+    </div>
+      
     </>
   );
 }
